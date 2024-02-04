4b065803c5d4a8134542467e09fa701c7a227477
or1is1 <0x7for1is1@gmail.com>
2024-02-05 03:05:12 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/or1is1/bartending/api/BartendingApiApplication.java
@@ -0,0 +1,13 @@
+package com.or1is1.bartending.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BartendingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BartendingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/or1is1/bartending/api/BartendingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.or1is1.bartending.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BartendingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
