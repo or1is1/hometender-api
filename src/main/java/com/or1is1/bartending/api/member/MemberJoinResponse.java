40f467e78a71a4099a8f03d898cf33335f65bd84
or1is1 <0x7for1is1@gmail.com>
2024-02-06 22:43:03 +0900

    feat: 비밀번호 암호화 적용

--- src/main/java/com/or1is1/bartending/api/member/MemberJoinResponse.java
+++ src/main/java/com/or1is1/bartending/api/member/MemberJoinResponse.java
@@ -3,12 +3,14 @@ package com.or1is1.bartending.api.member;
 
 public record MemberJoinResponse(
         Long id,
-        String nickname
+        String nickname,
+        String password
 ) {
     public MemberJoinResponse(Member member) {
         this(
                 member.getId(),
-                member.getNickname()
+                member.getNickname(),
+                member.getPassword()
         );
     }
 }
