76160af0257918613b31ccf53d1813be49fcf41e
or1is1 <0x7for1is1@gmail.com>
2024-02-08 00:57:54 +0900

    test: 비정상 루틴에 대한 테스트 코드 보완
    비정상 루틴 진행 시, 예외에 대한 정보를 받아와서 예상되는 결과와 비교하여 검증 테스트

--- src/test/java/com/or1is1/bartending/api/member/MemberControllerTest.java
+++ src/test/java/com/or1is1/bartending/api/member/MemberControllerTest.java
@@ -2,192 +2,257 @@ package com.or1is1.bartending.api.member;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@Transactional
+@WebMvcTest(controllers = {MemberController.class}, excludeAutoConfiguration = SecurityAutoConfiguration.class)
 class MemberControllerTest {
+    private final ObjectMapper objectMapper;
+    private final String email;
+    private final String password;
+    private final String nickname;
+    private final String url;
 
+    @MockBean
+    private MemberService memberService;
     @Autowired
     private MockMvc mockMvc;
 
 
     // 공통 사용 요소 초기화
-    private final ObjectMapper objectMapper = new ObjectMapper();
-    private final String email = "gildong@gmail.com";
-    private final String password = "hong1443";
-    private final String nickname = "홍길동";
-    private final String url = "/api/members";
+    public MemberControllerTest() {
+        objectMapper = new ObjectMapper();
+        email = "gildong@gmail.com";
+        password = "hong1443";
+        nickname = "홍길동";
+        url = "/api/members";
+    }
 
     @Test
-    @DisplayName("회원가입 정상처리")
+    @DisplayName("정상적인 회원가입 처리")
     void join() throws Exception {
         // given
         MemberJoinRequest memberJoinRequest = new MemberJoinRequest(email, password, nickname);
         String content = objectMapper.writeValueAsString(memberJoinRequest);
 
         // when
-        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.post(url)
+        when(memberService.join(any(MemberJoinRequest.class)))
+                .thenReturn(new MemberJoinResponse(1L, nickname));
+
+        ResultActions resultActions = mockMvc.perform(post(url)
                 .contentType(APPLICATION_JSON)
                 .content(content));
 
         //then
-        result.andExpectAll(
-                content().contentType(APPLICATION_JSON),
-                status().isOk(),
-                jsonPath("$.nickname").value(nickname)
-        );
+        resultActions.andExpect(status().isOk())
+                .andExpect(jsonPath("$.nickname").value(nickname));
     }
 
     @Test
-    @DisplayName("잘못된 이메일 형식으로 회원가입 시도")
+    @DisplayName("올바른 형식의 이메일로 회원가입을 해야 한다.")
     void joinWithWrongEmail() throws Exception {
         // given
         String email = "gildong#gmail.com";
-
         MemberJoinRequest memberJoinRequest = new MemberJoinRequest(email, password, nickname);
         String content = objectMapper.writeValueAsString(memberJoinRequest);
 
         // when
-        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.post(url)
+        ResultActions resultActions = mockMvc.perform(post(url)
                 .contentType(APPLICATION_JSON)
                 .content(content));
 
         //then
-        result.andExpectAll(
-                status().isBadRequest()
-        );
+        MvcResult mvcResult = resultActions.andExpect(status().isBadRequest())
+                .andReturn();
+
+        Exception resolvedException = mvcResult.getResolvedException();
+        assertThat(resolvedException).isExactlyInstanceOf(MethodArgumentNotValidException.class);
+
+        BindingResult bindingResult = ((MethodArgumentNotValidException) resolvedException).getBindingResult();
+        assertThat(bindingResult.getErrorCount()).isEqualTo(1);
+
+        ObjectError objectError = bindingResult.getAllErrors().get(0);
+        assertThat(objectError.getCode()).isEqualTo("Email");
+        assertThat(objectError.getDefaultMessage()).isEqualTo("잘못된 이메일 형식입니다.");
     }
 
     @Test
-    @DisplayName("너무 짧은 닉네임으로 회원가입 시도")
-    void joinWithTooShortNickname() throws Exception {
-        // given
-        String nickname = "홍";
-
-        MemberJoinRequest memberJoinRequest = new MemberJoinRequest(email, password, nickname);
-        String content = new ObjectMapper().writeValueAsString(memberJoinRequest);
-
-        // when
-        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.post("/api/members")
-                .contentType(APPLICATION_JSON)
-                .content(content));
-
-        //then
-        result.andExpectAll(
-                status().isBadRequest()
-        );
-    }
-
-    @Test
-    @DisplayName("너무 긴 닉네임으로 회원가입 시도")
-    void joinWithTooLongNickname() throws Exception {
-        // given
-        String nickname = "홍길동홍길동홍길동홍길동홍길동홍길동홍길동홍길동";
-
-        MemberJoinRequest memberJoinRequest = new MemberJoinRequest(email, password, nickname);
-        String content = new ObjectMapper().writeValueAsString(memberJoinRequest);
-
-        // when
-        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.post("/api/members")
-                .contentType(APPLICATION_JSON)
-                .content(content));
-
-        //then
-        result.andExpectAll(
-                status().isBadRequest()
-        );
-    }
-
-    @Test
-    @DisplayName("공백 닉네임으로 회원가입 시도")
-    void joinWithEmptyNickname() throws Exception {
-        // given
-        String nickname = "      ";
-
-        MemberJoinRequest memberJoinRequest = new MemberJoinRequest(email, password, nickname);
-        String content = new ObjectMapper().writeValueAsString(memberJoinRequest);
-
-        // when
-        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.post("/api/members")
-                .contentType(APPLICATION_JSON)
-                .content(content));
-
-        //then
-        result.andExpectAll(
-                status().isBadRequest()
-        );
-    }
-
-    @Test
-    @DisplayName("너무 짧은 비밀번호로 회원가입 시도")
-    void joinWithTooShortPassword() throws Exception {
-        // given
-        String password = "ho3";
-
-        MemberJoinRequest memberJoinRequest = new MemberJoinRequest(email, password, nickname);
-        String content = new ObjectMapper().writeValueAsString(memberJoinRequest);
-
-        // when
-        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.post("/api/members")
-                .contentType(APPLICATION_JSON)
-                .content(content));
-
-        //then
-        result.andExpectAll(
-                status().isBadRequest()
-        );
-    }
-
-    @Test
-    @DisplayName("너무 긴 비밀번호로 회원가입 시도")
-    void joinWithTooLongPassword() throws Exception {
-        // given
-        String password = "hong1443hong1443hong1443hong1443";
-
-        MemberJoinRequest memberJoinRequest = new MemberJoinRequest(email, password, nickname);
-        String content = new ObjectMapper().writeValueAsString(memberJoinRequest);
-
-        // when
-        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.post("/api/members")
-                .contentType(APPLICATION_JSON)
-                .content(content));
-
-        //then
-        result.andExpectAll(
-                status().isBadRequest()
-        );
-    }
-
-    @Test
-    @DisplayName("공백 비밀번호로 회원가입 시도")
+    @DisplayName("비밀번호는 비어 있으면 안된다.")
     void joinWithEmptyPassword() throws Exception {
         // given
         String password = "      ";
-
         MemberJoinRequest memberJoinRequest = new MemberJoinRequest(email, password, nickname);
         String content = new ObjectMapper().writeValueAsString(memberJoinRequest);
 
         // when
-        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.post("/api/members")
+        ResultActions resultActions = mockMvc.perform(post("/api/members")
                 .contentType(APPLICATION_JSON)
                 .content(content));
 
         //then
-        result.andExpectAll(
-                status().isBadRequest()
-        );
+        MvcResult mvcResult = resultActions.andExpect(status().isBadRequest())
+                .andReturn();
+        Exception resolvedException = mvcResult.getResolvedException();
+        assertThat(resolvedException).isExactlyInstanceOf(MethodArgumentNotValidException.class);
+
+        BindingResult bindingResult = ((MethodArgumentNotValidException) resolvedException).getBindingResult();
+        assertThat(bindingResult.getErrorCount()).isEqualTo(1);
+
+        ObjectError objectError = bindingResult.getAllErrors().get(0);
+        assertThat(objectError.getCode()).isEqualTo("NotBlank");
+        assertThat(objectError.getDefaultMessage()).isEqualTo("공백은 허용되지 않습니다.");
+    }
+
+    @Test
+    @DisplayName("비밀번호는 4글자보다 길어야 한다.")
+    void joinWithTooShortPassword() throws Exception {
+        // given
+        String password = "ho3";
+        MemberJoinRequest memberJoinRequest = new MemberJoinRequest(email, password, nickname);
+        String content = new ObjectMapper().writeValueAsString(memberJoinRequest);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(post("/api/members")
+                .contentType(APPLICATION_JSON)
+                .content(content));
+
+        //then
+        MvcResult mvcResult = resultActions.andExpect(status().isBadRequest())
+                .andReturn();
+        Exception resolvedException = mvcResult.getResolvedException();
+        assertThat(resolvedException).isExactlyInstanceOf(MethodArgumentNotValidException.class);
+
+        BindingResult bindingResult = ((MethodArgumentNotValidException) resolvedException).getBindingResult();
+        assertThat(bindingResult.getErrorCount()).isEqualTo(1);
+
+        ObjectError objectError = bindingResult.getAllErrors().get(0);
+        assertThat(objectError.getCode()).isEqualTo("Size");
+        assertThat(objectError.getDefaultMessage()).isEqualTo("비밀번호는 4자 이상, 20자 미만이여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("비밀번호는 20자보다 짧아야 한다.")
+    void joinWithTooLongPassword() throws Exception {
+        // given
+        String password = "hong1443hong1443hong14";
+        MemberJoinRequest memberJoinRequest = new MemberJoinRequest(email, password, nickname);
+        String content = new ObjectMapper().writeValueAsString(memberJoinRequest);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(post("/api/members")
+                .contentType(APPLICATION_JSON)
+                .content(content));
+
+        //then
+        MvcResult mvcResult = resultActions.andExpect(status().isBadRequest())
+                .andReturn();
+        Exception resolvedException = mvcResult.getResolvedException();
+        assertThat(resolvedException).isExactlyInstanceOf(MethodArgumentNotValidException.class);
+
+        BindingResult bindingResult = ((MethodArgumentNotValidException) resolvedException).getBindingResult();
+        assertThat(bindingResult.getErrorCount()).isEqualTo(1);
+
+        ObjectError objectError = bindingResult.getAllErrors().get(0);
+        assertThat(objectError.getCode()).isEqualTo("Size");
+        assertThat(objectError.getDefaultMessage()).isEqualTo("비밀번호는 4자 이상, 20자 미만이여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("닉네임은 비어있으면 안된다.")
+    void joinWithEmptyNickname() throws Exception {
+        // given
+        String nickname = "      ";
+        MemberJoinRequest memberJoinRequest = new MemberJoinRequest(email, password, nickname);
+        String content = new ObjectMapper().writeValueAsString(memberJoinRequest);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(post("/api/members")
+                .contentType(APPLICATION_JSON)
+                .content(content));
+
+        //then
+        MvcResult mvcResult = resultActions.andExpect(status().isBadRequest())
+                .andReturn();
+        Exception resolvedException = mvcResult.getResolvedException();
+        assertThat(resolvedException).isExactlyInstanceOf(MethodArgumentNotValidException.class);
+
+        BindingResult bindingResult = ((MethodArgumentNotValidException) resolvedException).getBindingResult();
+        assertThat(bindingResult.getErrorCount()).isEqualTo(1);
+
+        ObjectError objectError = bindingResult.getAllErrors().get(0);
+        assertThat(objectError.getCode()).isEqualTo("NotBlank");
+        assertThat(objectError.getDefaultMessage()).isEqualTo("공백은 허용되지 않습니다.");
+    }
+
+    @Test
+    @DisplayName("닉네임의 길이는 2자보다 길어야 한다.")
+    void joinWithTooShortNickname() throws Exception {
+        // given
+        String nickname = "홍";
+        MemberJoinRequest memberJoinRequest = new MemberJoinRequest(email, password, nickname);
+        String content = new ObjectMapper().writeValueAsString(memberJoinRequest);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(post("/api/members")
+                .contentType(APPLICATION_JSON)
+                .content(content));
+
+        //then
+        MvcResult mvcResult = resultActions.andExpect(status().isBadRequest())
+                .andReturn();
+        Exception resolvedException = mvcResult.getResolvedException();
+        assertThat(resolvedException).isExactlyInstanceOf(MethodArgumentNotValidException.class);
+
+        BindingResult bindingResult = ((MethodArgumentNotValidException) resolvedException).getBindingResult();
+        assertThat(bindingResult.getErrorCount()).isEqualTo(1);
+
+        ObjectError objectError = bindingResult.getAllErrors().get(0);
+        assertThat(objectError.getCode()).isEqualTo("Size");
+        assertThat(objectError.getDefaultMessage()).isEqualTo("닉네임은 2자 이상, 10자 미만이여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("닉네임의 길이는 10자보다 짧아야 한다")
+    void joinWithTooLongNickname() throws Exception {
+        // given
+        String nickname = "홍길동홍길동홍길동홍길";
+        MemberJoinRequest memberJoinRequest = new MemberJoinRequest(email, password, nickname);
+        String content = new ObjectMapper().writeValueAsString(memberJoinRequest);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(post("/api/members")
+                .contentType(APPLICATION_JSON)
+                .content(content));
+
+        //then
+        MvcResult mvcResult = resultActions.andExpect(status().isBadRequest())
+                .andReturn();
+        Exception resolvedException = mvcResult.getResolvedException();
+        assertThat(resolvedException).isExactlyInstanceOf(MethodArgumentNotValidException.class);
+
+        BindingResult bindingResult = ((MethodArgumentNotValidException) resolvedException).getBindingResult();
+        assertThat(bindingResult.getErrorCount()).isEqualTo(1);
+
+        ObjectError objectError = bindingResult.getAllErrors().get(0);
+        assertThat(objectError.getCode()).isEqualTo("Size");
+        assertThat(objectError.getDefaultMessage()).isEqualTo("닉네임은 2자 이상, 10자 미만이여야 합니다.");
     }
 }
