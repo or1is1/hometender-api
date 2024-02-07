f1a2502e465eb28d2acf2ff96c28f9be6db67a1c
or1is1 <0x7for1is1@gmail.com>
2024-02-08 08:19:50 +0900

    chore: swagger 적용

---
+++ src/main/java/com/or1is1/bartending/api/configuration/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package com.or1is1.bartending.api.configuration;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+	@Bean
+	public GroupedOpenApi customApi() {
+		return GroupedOpenApi.builder()
+				.group("bartending-api") // API 그룹명
+				.pathsToMatch("/api/**") // API 패스 패턴
+				.build();
+	}
+}
