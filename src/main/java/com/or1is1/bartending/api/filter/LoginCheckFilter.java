7d23bd6b8a8bcd6bbce00051f9ebd94f19a78d59
or1is1 <0x7for1is1@gmail.com>
2024-02-16 08:15:18 +0900

    fix: 로그인 체크 필터를 수정하여, swagger-ui 를 볼 수 없던 문제 해결

--- src/main/java/com/or1is1/bartending/api/filter/LoginCheckFilter.java
+++ src/main/java/com/or1is1/bartending/api/filter/LoginCheckFilter.java
@@ -12,17 +12,23 @@ import org.springframework.context.MessageSource;
 import org.springframework.util.PatternMatchUtils;
 
 import java.io.IOException;
-import java.util.Locale;
 
 import static com.or1is1.bartending.api.StringConst.LOGIN_MEMBER;
 import static jakarta.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Locale.KOREAN;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Slf4j
 @RequiredArgsConstructor
 public class LoginCheckFilter implements Filter {
-	private static final String[] whitelist = {"/", "/api/members/join", "/api/members/login", "/css/*"};
+	private static final String[] whitelist = {
+			"/api/members/join",
+			"/api/members/login",
+			"/swagger-ui/*",
+			"/v3/api-docs/bartending-api",
+			"/v3/api-docs/swagger-config"
+	};
 	private final MessageSource messageSource;
 	private ObjectMapper objectMapper;
 
@@ -34,15 +40,18 @@ public class LoginCheckFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-		HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+		String requestURI = httpServletRequest.getRequestURI();
+		HttpSession session = httpServletRequest.getSession(false);
 
-		if (!isAuthenticated(httpServletRequest)) {
-			String message = messageSource.getMessage("member.exception.notAuthenticated", null, Locale.KOREAN);
+		if (!isAuthenticated(requestURI, session)) {
+			String message = messageSource.getMessage("member.exception.notAuthenticated", null, KOREAN);
+
+			log.info("{} | requestUri = {} | sessionIsNull = {}", message, requestURI, session == null);
+
 			CommonResponse<Void> objectCommonResponse = new CommonResponse<>(message, null);
-
-			log.info("{} | sessionIsNull = {}", message, httpServletRequest.getSession(false) == null);
-
 			String content = objectMapper.writeValueAsString(objectCommonResponse);
+
+			HttpServletResponse httpServletResponse = (HttpServletResponse) response;
 			httpServletResponse.setContentType(APPLICATION_JSON_VALUE);
 			httpServletResponse.setCharacterEncoding(UTF_8.name());
 			httpServletResponse.setStatus(SC_BAD_REQUEST);
@@ -54,14 +63,10 @@ public class LoginCheckFilter implements Filter {
 		chain.doFilter(request, response);
 	}
 
-	private boolean isAuthenticated(HttpServletRequest httpServletRequest) {
-		String requestURI = httpServletRequest.getRequestURI();
-
+	private boolean isAuthenticated(String requestURI, HttpSession session) {
 		if (PatternMatchUtils.simpleMatch(whitelist, requestURI)) {
 			return true;
 		}
-
-		HttpSession session = httpServletRequest.getSession(false);
 
 		return session != null && session.getAttribute(LOGIN_MEMBER) != null;
 	}
