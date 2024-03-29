a5e7dc23136604a9f59c5a0430f1363f9d6b69d2
or1is1 <0x7for1is1@gmail.com>
2024-03-29 14:03:26 +0900

    feat: QueryDsl 용 JPAQueryFactory 객체를 Spring Bean 으로 등록

--- src/main/java/com/or1is1/hometender/api/domain/member/MemberRepositoryImpl.java
+++ src/main/java/com/or1is1/hometender/api/domain/member/MemberRepositoryImpl.java
@@ -1,21 +1,15 @@
 package com.or1is1.hometender.api.domain.member;
 
-import com.or1is1.hometender.api.dto.IsExistMemberResponse;
 import com.or1is1.hometender.api.domain.member.dto.QIsExistMemberResponse;
+import com.or1is1.hometender.api.dto.IsExistMemberResponse;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 
 import static com.or1is1.hometender.api.domain.member.QMember.member;
 
-
+@RequiredArgsConstructor
 public class MemberRepositoryImpl implements MemberRepositoryInterface {
 	private final JPAQueryFactory jpaQueryFactory;
-
-	@Autowired
-	public MemberRepositoryImpl(EntityManager entityManager) {
-		this.jpaQueryFactory = new JPAQueryFactory(entityManager);
-	}
 
 	@Override
 	public IsExistMemberResponse isExists(String loginId, String nickname) {
